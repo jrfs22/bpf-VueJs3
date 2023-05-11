f87f4ffe0028559403237521e48d1e70fe2dde26
jrfs22 <mr.jrfs22@gmail.com>
2023-05-11 22:42:13 +0700

    ADD BPF2 ~ VueJS3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Price from './components/Price.vue'
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h1>ID Shop</h1>
+    <p class="animate_animated animate_fadeInRigth">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis adipisci, beatae dolor laboriosam odit doloribus sapiente rem, aperiam mollitia, unde culpa tempora exercitationem incidunt ipsam nobis? Consectetur deleniti perferendis minima.
+    </p>
+    <Price value="4.23"/>
+  </div>
+</template>
+
+
---
+++ src/components/Price.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    value: Number,
+    prefix: {
+        type: String,
+        default: '$'
+    },
+    precission:{
+        type: Number,
+        default: 2
+    }
+})
+</script>
+<template>
+    <span>{{ prefix + Number.parseFloat(value).toFixed(precission) }}</span>
+</template>
